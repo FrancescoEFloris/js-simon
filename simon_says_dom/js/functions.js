c877b54f4fe4fb72567fdcc29a8803bc158525d5
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-07 17:01:14 +0200

    aggiunti timer e form alla fine del timer

--- simon_says_dom/js/functions.js
+++ simon_says_dom/js/functions.js
@@ -1,10 +1,9 @@
 
 // Generatore numeri random da ricordare
-
 function generaNumeri() {
     for (let i = 0; i < 5; i++) {
         // - genero numer random
-        let numeroRandom = Math.ceil(Math.random() * 100);
+        let numeroRandom = Math.ceil(Math.random() * 50);
 
         // - se il numero random è già presente nella lista
         //   ne genero un altro, se non è presente lo aggiungo
@@ -15,4 +14,21 @@ function generaNumeri() {
         }
     }
 }
+
+// Tempo rimasto per memorizzare i numeri
+function timer() {
+    if (tempoRimasto > 0) {
+        tempoRimasto--;
+        countdown.innerHTML = `Tempo rimasto: ${tempoRimasto} secondi`;
+        // console.log(tempoRimasto);
+        setTimeout(timer, 1000);
+    } else {
+        countdown.innerHTML = "Inserisci i numeri che ricordi!";
+        listaNumeri.classList.add("d-none");
+        formUtente.classList.remove("d-none");
+    }
+}
+
+// Salva i dati inseriti dall'utente
+
 
--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,6 +1,18 @@
+// Numeri da ricordare
 let numeriGenerati = [];
+let listaNumeri = document.querySelector("#numbers-list");
+// Numeri dell'utente
 let numeriUtente = [];
+let formUtente = document.querySelector("#answers-form");
+let inputUtente = document.querySelectorAll(".form-control");;
+// Timer
+let countdown = document.querySelector("#countdown");
+let tempoRimasto = 4;
 
 generaNumeri();
-// console.log(numeriGenerati);
+listaNumeri.innerHTML = "<li>" + numeriGenerati[0] + "</li><li>" + numeriGenerati[1] + "</li><li>" + numeriGenerati[2] + "</li><li>" + numeriGenerati[3] + "</li><li>" + numeriGenerati[4] + "</li>";
+console.log(numeriGenerati);
+
+timer();
+
 
